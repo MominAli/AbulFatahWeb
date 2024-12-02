0bea0cbb75c4c88e8ab8350480e294cae7da12f8
MominAli <momin.ali7514@gmail.com>
2024-12-02 22:45:33 +0530

    setup pdf viewer

--- client/src/app/quran/quran-parawise/quran-parawise.component.ts
+++ client/src/app/quran/quran-parawise/quran-parawise.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { NgxExtendedPdfViewerModule } from 'ngx-extended-pdf-viewer';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-quran-parawise',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet, NgxExtendedPdfViewerModule],
+
   templateUrl: './quran-parawise.component.html',
   styleUrl: './quran-parawise.component.css'
 })
