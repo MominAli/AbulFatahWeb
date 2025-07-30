ee5ce56882ba654af701a5985891ff670d6caabc
MominAli <momin.ali7514@gmail.com>
2025-07-30 18:26:34 +0530

    books page craeted

--- client/src/app/customer/services/bookdetails.service.ts
+++ client/src/app/customer/services/bookdetails.service.ts
@@ -1,9 +1,35 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface SubBook {
+  id: number;
+  title: string;
+}
+
+export interface Book {
+  id: number;
+  title: string;
+  hasChildren: boolean;
+  img: string;
+  subBooks: SubBook[];
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
+
 export class BookdetailsService {
 
-  constructor() { }
+  private dataUrl = 'assets/books.json'; // Path to the JSON file
+
+  constructor(private http: HttpClient) { }
+
+  getBookList(): Observable<any> {
+    debugger;
+    console.log('Fetching books.json from:', this.dataUrl);
+    return this.http.get<any>(this.dataUrl);
+  }
+
 }
